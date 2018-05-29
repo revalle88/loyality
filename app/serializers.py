c261be8e7be2f3fc2d78c38b117b94ee1cad9ac4
revalle88 <revalle88@gmail.com>
2018-05-29 11:09:32 +0300

    summ serializer

--- app/serializers.py
+++ app/serializers.py
@@ -4,4 +4,4 @@ from .models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('card', 'first_name','last_name', 'patronymic', 'bdate', 'phone')
+        fields = ('card', 'first_name','last_name', 'patronymic', 'bdate', 'phone', 'summ')
